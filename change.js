af76e49563cedbc659a4b9f2dec569644ac5ed71
annshkatova <annash2007@gmail.com>
2024-11-29 00:33:25 +0300

    change.js
    Signed-off-by: annshkatova <annash2007@gmail.com>

--- change.js
+++ change.js
@@ -1,21 +1,21 @@
 const change = document.getElementById("change")
 
 function change_all(){
-    if (document.getElementsByTagName('div')[3].innerText==='Shkatova'){
-        document.getElementsByTagName('div')[3].innerText='Шкатова';
-        document.getElementsByTagName('div')[4].innerText='Анна';
-        document.getElementsByTagName('div')[5].innerText='Юрьевна';
-        document.getElementsByTagName('div')[6].innerText='жен.';
-        document.getElementsByTagName('div')[8].innerText='гор. Москва';
+    if (document.getElementsByTagName('div')[7].innerText==='Shkatova'){
+        document.getElementsByTagName('div')[7].innerText='Шкатова';
+        document.getElementsByTagName('div')[8].innerText='Анна';
+        document.getElementsByTagName('div')[9].innerText='Юрьевна';
+        document.getElementsByTagName('div')[10].innerText='жен.';
+        document.getElementsByTagName('div')[12].innerText='гор. Москва';
     }
     else{
 
-        if (document.getElementsByTagName('div')[3].innerText==='Шкатова'){
-            document.getElementsByTagName('div')[3].innerText='Shkatova';
-            document.getElementsByTagName('div')[4].innerText='Anna';
-            document.getElementsByTagName('div')[5].innerText='Yurievna';
-            document.getElementsByTagName('div')[6].innerText='female';
-            document.getElementsByTagName('div')[8].innerText='Moscow';
+        if (document.getElementsByTagName('div')[7].innerText==='Шкатова'){
+            document.getElementsByTagName('div')[7].innerText='Shkatova';
+            document.getElementsByTagName('div')[8].innerText='Anna';
+            document.getElementsByTagName('div')[9].innerText='Yurievna';
+            document.getElementsByTagName('div')[10].innerText='female';
+            document.getElementsByTagName('div')[12].innerText='Moscow';
 
         }
     }
